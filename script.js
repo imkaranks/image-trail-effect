02a8de789668f6906ca1ae671cee3545f7443823
Karan <risingphoenix160@gmail.com>
2023-09-16 21:27:10 +0530

    Add additional effects

--- script.js
+++ script.js
@@ -11,11 +11,14 @@
     'https://images.unsplash.com/photo-1583119912267-cc97c911e416?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8cGFpbnRpbmd8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60'
   ];
   let currentImage = -1;
+  const trailDelay = 150;
 
   if (window.matchMedia('(pointer: fine)').matches) {
     $heroSection.addEventListener(
-      'mousemove', throttleFn(imageTrail, 150)
+      'mousemove', throttleFn(imageTrail, trailDelay)
     );
+  } else {
+    alert('Please view on desktop 😢');
   }
 
   function getNewImage() {
@@ -42,30 +45,129 @@
     const $imgWrapper = document.createElement('div');
     const $img = document.createElement('img');
 
-    $imgWrapper.className = 'img__wrapper';
-    $imgWrapper.style.top = event.clientY - 125 + 'px';
-    $imgWrapper.style.left = event.clientX - 100 + 'px';
-    $imgWrapper.style.transform = `rotate(${(Math.floor(Math.random() * 10)) * (Math.round(Math.random()) ? 1 : -1)}deg)`;
+    // $imgWrapper.className = 'img__wrapper variant-1';
+    $imgWrapper.className = 'img__wrapper variant-2';
+    // $imgWrapper.className = 'img__wrapper variant-3';
+    // $imgWrapper.className = 'img__wrapper variant-4';
+    // $imgWrapper.className = 'img__wrapper variant-5';
+
+    function setImagePositon(event) {
+      setTimeout(() => {
+        $imgWrapper.style.top = event.clientY - 125 + 'px';
+        $imgWrapper.style.left = event.clientX - 100 + 'px';
+      }, trailDelay);
+    }
+
+    document.addEventListener('mousemove', setImagePositon);
+    // Optional style for variant 1
+    // $imgWrapper.style.transform = `rotate(${(Math.floor(Math.random() * 10)) * (Math.round(Math.random()) ? 1 : -1)}deg)`;
     $img.src = getNewImage();
 
+    /* ===[[ variant 1 ]]=== */
+    // gsap.to($img, {
+    //   y: 0,
+    //   ease: 'power2',
+    //   duration: 1
+    // });
+
+    // gsap.to($img, {
+    //   y: '100%',
+    //   ease: 'power1',
+    //   delay: 1,
+    //   duration: 1
+    // });
+
+    /* ===[[ variant 2 ]]=== */
     gsap.to($img, {
-      y: 0,
+      opacity: 1,
       ease: 'power2',
-      duration: 0.5
+      duration: 1
     });
 
-    gsap.to($img, {
-      y: '100%',
+    gsap.to($imgWrapper, {
+      opacity: 0,
       ease: 'power1',
       delay: 0.5,
-      duration: 0.5
+      duration: 1
     });
+
+    gsap.to($imgWrapper, {
+      scale: 3,
+      ease: 'power1',
+      delay: 1,
+      duration: 1
+    });
+
+    /* ===[[ variant 3 ]]=== */
+    // gsap.to($img, {
+    //   opacity: 1,
+    //   ease: 'power2',
+    //   duration: 1
+    // });
+
+    // gsap.to($imgWrapper, {
+    //   opacity: 0,
+    //   scale: 0,
+    //   ease: 'power1',
+    //   delay: 1,
+    //   duration: 1
+    // });
+
+    /* ===[[ variant 4 ]]=== */
+    // gsap.to($img, {
+    //   opacity: 1,
+    //   ease: 'power2',
+    //   duration: 1
+    // });
+
+    // gsap.to($img, {
+    //   opacity: 0,
+    //   ease: 'power1',
+    //   delay: 0.5,
+    //   duration: 1
+    // });
+
+    // gsap.to($imgWrapper, {
+    //   rotate: '45deg',
+    //   top: (Math.floor(Math.random() * (100 - 2)) + 2) * (Math.round(Math.random()) ? 1 : -1) + 'rem',
+    //   left: (Math.floor(Math.random() * (100 - 2)) + 2) * (Math.round(Math.random()) ? 1 : -1) + 'rem',
+    //   ease: 'power1',
+    //   delay: 1,
+    //   duration: 1
+    // });
+    
+    /* ===[[ variant 5 ]]=== */
+    // gsap.to($img, {
+    //   opacity: 1,
+    //   ease: 'power2',
+    //   duration: 1
+    // });
+
+    // gsap.to($imgWrapper, {
+    //   opacity: 0,
+    //   ease: 'power1',
+    //   delay: 0.5,
+    //   duration: 1
+    // });
+
+    // gsap.to($imgWrapper, {
+    //   scaleX: 0,
+    //   scaleY: 1.4,
+    //   y: '100vmax',
+    //   ease: 'power1',
+    //   delay: 1,
+    //   duration: 1
+    // });
 
     $imgWrapper.appendChild($img);
     document.body.appendChild($imgWrapper);
 
-    setTimeout(() =>
+    setTimeout(() => {
+      document.removeEventListener('mousemove',  setImagePositon);
+    }, trailDelay);
+
+    setTimeout(() => {
       $imgWrapper.remove()
-    , 1000);
+    }, 2000);
   }
 })();
