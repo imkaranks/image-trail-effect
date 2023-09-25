9ad50a178fbede0606240a6b86f659bed1e71d59
Karan <risingphoenix160@gmail.com>
2023-09-25 21:32:05 +0530

    fix stutter and reduce animation duration

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 (function () {
-  const $heroSection = document.querySelector('.hero');
   const images = [
     'https://images.unsplash.com/flagged/photo-1572392640988-ba48d1a74457?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8YXJ0fGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60',
     'https://images.unsplash.com/photo-1578926375605-eaf7559b1458?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8cGFpbnRpbmdzfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60',
@@ -10,15 +9,27 @@
     'https://images.unsplash.com/photo-1549289524-06cf8837ace5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8cGFpbnRpbmd8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60',
     'https://images.unsplash.com/photo-1583119912267-cc97c911e416?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8cGFpbnRpbmd8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60'
   ];
-  let currentImage = -1;
+  const $variantSelect = document.getElementById('effect-variant');
   const trailDelay = 150;
+  let currentImage = -1;
+  let variant = +$variantSelect.value;
+
+  $variantSelect.onchange = function (e) {
+    if (variant !== e.target.value) {
+      variant = +e.target.value;
+    }
+  }
 
   if (window.matchMedia('(pointer: fine)').matches) {
-    $heroSection.addEventListener(
+    document.addEventListener(
       'mousemove', throttleFn(imageTrail, trailDelay)
     );
   } else {
     alert('Please view on desktop 😢');
+  }
+
+  function getRandInt(min, max) {
+    return (Math.floor(Math.random() * (max - min)) + min) * (Math.round(Math.random()) ? 1 : -1);
   }
 
   function getNewImage() {
@@ -45,119 +56,125 @@
     const $imgWrapper = document.createElement('div');
     const $img = document.createElement('img');
 
-    // $imgWrapper.className = 'img__wrapper variant-1';
-    $imgWrapper.className = 'img__wrapper variant-2';
-    // $imgWrapper.className = 'img__wrapper variant-3';
-    // $imgWrapper.className = 'img__wrapper variant-4';
-    // $imgWrapper.className = 'img__wrapper variant-5';
+    $imgWrapper.className = 'img__wrapper variant-' + variant;
+
+    $imgWrapper.style.top = event.clientY + 'px';
+    $imgWrapper.style.left = event.clientX + 'px';
 
     function setImagePositon(event) {
+      gsap.to($imgWrapper, {
+        top: event.clientY + 'px',
+        left: event.clientX + 'px',
+        stagger: 0.3,
+      });
       setTimeout(() => {
-        $imgWrapper.style.top = event.clientY - 125 + 'px';
-        $imgWrapper.style.left = event.clientX - 100 + 'px';
       }, trailDelay);
     }
 
     document.addEventListener('mousemove', setImagePositon);
     // Optional style for variant 1
-    // $imgWrapper.style.transform = `rotate(${(Math.floor(Math.random() * 10)) * (Math.round(Math.random()) ? 1 : -1)}deg)`;
+    // $imgWrapper.style.transform = `rotate(${getRandInt(0, 10)}deg)`;
     $img.src = getNewImage();
 
-    /* ===[[ variant 1 ]]=== */
-    // gsap.to($img, {
-    //   y: 0,
-    //   ease: 'power2',
-    //   duration: 1
-    // });
-
-    // gsap.to($img, {
-    //   y: '100%',
-    //   ease: 'power1',
-    //   delay: 1,
-    //   duration: 1
-    // });
-
-    /* ===[[ variant 2 ]]=== */
-    gsap.to($img, {
-      opacity: 1,
-      ease: 'power2',
-      duration: 1
-    });
-
-    gsap.to($imgWrapper, {
-      opacity: 0,
-      ease: 'power1',
-      delay: 0.5,
-      duration: 1
-    });
-
-    gsap.to($imgWrapper, {
-      scale: 3,
-      ease: 'power1',
-      delay: 1,
-      duration: 1
-    });
-
-    /* ===[[ variant 3 ]]=== */
-    // gsap.to($img, {
-    //   opacity: 1,
-    //   ease: 'power2',
-    //   duration: 1
-    // });
-
-    // gsap.to($imgWrapper, {
-    //   opacity: 0,
-    //   scale: 0,
-    //   ease: 'power1',
-    //   delay: 1,
-    //   duration: 1
-    // });
-
-    /* ===[[ variant 4 ]]=== */
-    // gsap.to($img, {
-    //   opacity: 1,
-    //   ease: 'power2',
-    //   duration: 1
-    // });
-
-    // gsap.to($img, {
-    //   opacity: 0,
-    //   ease: 'power1',
-    //   delay: 0.5,
-    //   duration: 1
-    // });
-
-    // gsap.to($imgWrapper, {
-    //   rotate: '45deg',
-    //   top: (Math.floor(Math.random() * (100 - 2)) + 2) * (Math.round(Math.random()) ? 1 : -1) + 'rem',
-    //   left: (Math.floor(Math.random() * (100 - 2)) + 2) * (Math.round(Math.random()) ? 1 : -1) + 'rem',
-    //   ease: 'power1',
-    //   delay: 1,
-    //   duration: 1
-    // });
+    /* ===[[ variants ]]=== */
+    switch (variant) {
+      case 1:
+        gsap.to($img, {
+          y: 0,
+          ease: 'power2',
+          duration: 0.5
+        });
     
-    /* ===[[ variant 5 ]]=== */
-    // gsap.to($img, {
-    //   opacity: 1,
-    //   ease: 'power2',
-    //   duration: 1
-    // });
-
-    // gsap.to($imgWrapper, {
-    //   opacity: 0,
-    //   ease: 'power1',
-    //   delay: 0.5,
-    //   duration: 1
-    // });
-
-    // gsap.to($imgWrapper, {
-    //   scaleX: 0,
-    //   scaleY: 1.4,
-    //   y: '100vmax',
-    //   ease: 'power1',
-    //   delay: 1,
-    //   duration: 1
-    // });
+        gsap.to($img, {
+          y: '100%',
+          ease: 'power1',
+          delay: 0.5,
+          duration: 0.5
+        });
+        break;
+      case 2:
+        gsap.to($img, {
+          opacity: 1,
+          ease: 'power2',
+          duration: 0.5
+        });
+    
+        gsap.to($imgWrapper, {
+          opacity: 0,
+          ease: 'power1',
+          delay: 0.25,
+          duration: 0.5
+        });
+    
+        gsap.to($imgWrapper, {
+          scale: 3,
+          ease: 'power1',
+          delay: 0.5,
+          duration: 0.5
+        });
+        break;
+      case 3:
+        gsap.to($img, {
+          opacity: 1,
+          ease: 'power2',
+          duration: 0.5
+        });
+    
+        gsap.to($imgWrapper, {
+          opacity: 0,
+          scale: 0,
+          ease: 'power1',
+          delay: 0.5,
+          duration: 0.5
+        });
+        break;
+      case 4:
+        gsap.to($img, {
+          opacity: 1,
+          ease: 'power2',
+          duration: 1
+        });
+    
+        gsap.to($img, {
+          opacity: 0,
+          ease: 'power1',
+          delay: 0.5,
+          duration: 1
+        });
+    
+        gsap.to($imgWrapper, {
+          rotate: getRandInt(0, 45) + 'deg',
+          top: getRandInt(2, 100) + 'rem',
+          left: getRandInt(2, 100) + 'rem',
+          ease: 'power1',
+          delay: 1,
+          duration: 1
+        });
+        break;
+      case 5:
+        gsap.to($img, {
+          opacity: 1,
+          ease: 'power2',
+          duration: 0.5
+        });
+    
+        gsap.to($imgWrapper, {
+          opacity: 0,
+          ease: 'power1',
+          delay: 0.25,
+          duration: 0.5
+        });
+    
+        gsap.to($imgWrapper, {
+          scaleX: 0,
+          scaleY: 1.4,
+          y: '100vmax',
+          ease: 'power1',
+          delay: 0.5,
+          duration: 0.5
+        });
+        break;
+    }
 
     $imgWrapper.appendChild($img);
     document.body.appendChild($imgWrapper);
@@ -168,6 +185,6 @@
 
     setTimeout(() => {
       $imgWrapper.remove()
-    }, 2000);
+    }, variant === 4 ? 2000 : 1000);
   }
 })();
